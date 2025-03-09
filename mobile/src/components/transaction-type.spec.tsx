41b9c0c12e1e0ecc8e558ae9e378dca719dff8ec
arthur-uzumaki <arthursousarabelo@gmail.com>
2025-03-09 10:58:16 -0300

    feat: adding component and test transaction-type

---
+++ mobile/src/components/transaction-type.spec.tsx
@@ -0,0 +1,19 @@
+import { MaterialIcons } from '@expo/vector-icons'
+import { render, screen } from '@testing-library/react-native'
+import { TransactionType } from './transaction-type'
+
+jest.mock('@expo/vector-icons', () => ({
+  MaterialIcons: jest.fn(() => null),
+}))
+describe('Component: TransactionType', () => {
+  it('should be render with transaction type', () => {
+    render(
+      <TransactionType
+        type={{ color: 'red', title: 'Depósito', selected: true, icon: 'add' }}
+      />
+    )
+    const transactionType = screen.getByTestId('transaction-type-id')
+
+    expect(transactionType).toBeTruthy()
+  })
+})
